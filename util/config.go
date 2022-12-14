7a56031918c1fafc1618e658918496f171e56ee3
raman <ramanvahedi83@gmail.com>
2022-12-14 12:41:43 +0330

    add config env variable loader

---
+++ util/config.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DB_DRIVER      string
+	DB_SOURCE      string
+	DB_SOURCE_TEST string
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Printf("can't read environment variables: %v\n", err)
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
