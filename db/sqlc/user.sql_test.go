7ab381c4fdfc55e47a1e4cb7d00f238037a74598
raman <ramanvahedi83@gmail.com>
2022-12-14 16:07:36 +0330

    add user sql queries unit testing

---
+++ db/sqlc/user.sql_test.go
@@ -0,0 +1,83 @@
+package sqlc
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/Vodnik-Project/vodnik-api/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createrandomuser(t *testing.T) User {
+	randomuser := CreateUserParams{
+		Username: util.RandomString(5),
+		Email:    util.RandomEmail(),
+		PassHash: util.RandomString(10),
+		Bio:      sql.NullString{String: util.RandomString(30), Valid: true},
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), randomuser)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, randomuser.Username, user.Username)
+	require.Equal(t, randomuser.Email, user.Email)
+	require.Equal(t, randomuser.PassHash, user.PassHash)
+	require.Equal(t, randomuser.Bio, user.Bio)
+
+	return user
+}
+func TestCreateUser(t *testing.T) {
+	createrandomuser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	randomuser := createrandomuser(t)
+	toGetBy := []GetUserParams{
+		{ID: randomuser.ID},
+		{Email: randomuser.Email},
+	}
+	for _, arg := range toGetBy {
+		user, err := testQueries.GetUser(context.Background(), arg)
+		require.NoError(t, err)
+		require.NotEmpty(t, user)
+
+		require.Equal(t, randomuser.Username, user.Username)
+		require.Equal(t, randomuser.Email, user.Email)
+		require.Equal(t, randomuser.PassHash, user.PassHash)
+		require.Equal(t, randomuser.Bio, user.Bio)
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	randomuser := createrandomuser(t)
+	newUsername := util.RandomString(5)
+	user, err := testQueries.UpdateUser(context.Background(), UpdateUserParams{
+		Username: newUsername,
+		ID:       randomuser.ID,
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, newUsername, user.Username)
+	require.Equal(t, randomuser.Email, user.Email)
+	require.Equal(t, randomuser.PassHash, user.PassHash)
+	require.Equal(t, randomuser.Bio, user.Bio)
+	require.Equal(t, randomuser.ID, user.ID)
+	require.Equal(t, randomuser.JoinDate, user.JoinDate)
+}
+
+func TestDeleteUser(t *testing.T) {
+	randomuser := createrandomuser(t)
+
+	err := testQueries.DeleteUser(context.Background(), randomuser.ID)
+	require.NoError(t, err)
+
+	user, err := testQueries.GetUser(context.Background(), GetUserParams{
+		ID: randomuser.ID,
+	})
+	require.Error(t, err)
+	require.EqualError(t, sql.ErrNoRows, err.Error())
+	require.Empty(t, user)
+}
