650b04d08e7b4b7b8be7d79f406edfcb575c8a36
raman <ramanvahedi83@gmail.com>
2022-12-15 16:12:50 +0330

    add main

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/Vodnik-Project/vodnik-api/api"
+	"github.com/Vodnik-Project/vodnik-api/db/sqlc"
+	"github.com/Vodnik-Project/vodnik-api/util"
+)
 
 func main() {
-	fmt.Println("Hey")
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("can't load env variables: %v", err)
+	}
+	db, err := sql.Open(config.DB_DRIVER, config.DB_SOURCE)
+	if err != nil {
+		log.Fatalf("can't connect to db: %v", err)
+	}
+	q := sqlc.New(db)
+	server := api.NewServer(q)
+	err = server.StartServer(config.SERVER_PORT)
+	if err != nil {
+		log.Fatalf("can't start server: %v", err)
+	}
 }
