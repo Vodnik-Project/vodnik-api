70bfa250b06257f604df9d81086082a7e4b77c04
raman <ramanvahedi83@gmail.com>
2022-12-16 13:05:52 +0330

    add jwt token maker

---
+++ auth/token.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+)
+
+type Token struct {
+	Secret               []byte
+	AccessTokenDuration  time.Duration
+	RefreshTokenDuration time.Duration
+}
+
+type TokenMaker interface {
+	CreateAccessToken(userID uuid.UUID) (string, error)
+	CreateRefreshToken() (string, error)
+}
+
+func NewTokenMaker(token Token) Token {
+	t := Token{
+		Secret:               token.Secret,
+		AccessTokenDuration:  token.AccessTokenDuration,
+		RefreshTokenDuration: token.RefreshTokenDuration,
+	}
+	return t
+}
+
+func (t Token) CreateAccessToken(userID uuid.UUID) (string, error) {
+	p := NewAccessTokenPayload(userID, t.AccessTokenDuration)
+	if err := p.Valid(); err != nil {
+		return "", fmt.Errorf("payload is not valid: %v", err)
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, p)
+	tk, err := token.SignedString(t.Secret)
+	if err != nil {
+		return "", fmt.Errorf("can't create token: %v", err)
+	}
+	return tk, nil
+}
+
+func (t Token) CreateRefreshToken() (string, error) {
+	p := NewRefreshTokenPayload(t.RefreshTokenDuration)
+	if err := p.Valid(); err != nil {
+		return "", fmt.Errorf("payload is not valid: %v", err)
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, p)
+	tk, err := token.SignedString(t.Secret)
+	if err != nil {
+		return "", fmt.Errorf("can't create token: %v", err)
+	}
+	return tk, nil
+}
