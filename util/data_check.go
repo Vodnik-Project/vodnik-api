edd35938b546f7f19aac0e3901a7f365e8385e27
raman <ramanvahedi83@gmail.com>
2022-12-16 17:34:47 +0330

    add password hash check util

--- util/data_check.go
+++ util/data_check.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"reflect"
 )
@@ -12,6 +14,14 @@ func CheckEmpty(s interface{}, args []string) error {
 		if v.FieldByName(i).String() == "" {
 			return fmt.Errorf("%v can't be empty", i)
 		}
+	}
+	return nil
+}
+
+func CheckPassword(password, passHash string) error {
+	inputPassHash := sha256.Sum256([]byte(password))
+	if hex.EncodeToString(inputPassHash[:]) != passHash {
+		return fmt.Errorf("password is wrong")
 	}
 	return nil
 }
