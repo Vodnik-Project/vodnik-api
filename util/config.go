a70641d0fc466df5a72035f078c48388dd0731c9
raman <ramanvahedi83@gmail.com>
2022-12-16 13:10:23 +0330

    add new env variables for jwt token

--- util/config.go
+++ util/config.go
@@ -2,18 +2,22 @@ package util
 
 import (
 	"log"
+	"time"
 
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	DB_DRIVER      string
-	DB_SOURCE      string
-	DB_SOURCE_TEST string
-	DB_USER        string
-	DB_NAME        string
-	DB_NAME_TEST   string
-	SERVER_PORT    string
+	DB_DRIVER              string
+	DB_SOURCE              string
+	DB_SOURCE_TEST         string
+	DB_USER                string
+	DB_NAME                string
+	DB_NAME_TEST           string
+	SERVER_PORT            string
+	JWT_SECRET_KEY         string
+	REFRESH_TOKEN_DURATION time.Duration
+	ACCESS_TOKEN_DURATION  time.Duration
 }
 
 func LoadConfig(path string) (config Config, err error) {
