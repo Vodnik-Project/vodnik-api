184c28430cf8f207b568794edd7b1d0eaef0744b
raman <ramanvahedi83@gmail.com>
2022-12-18 16:33:28 +0330

    add GetUsername util

--- util/session.go
+++ util/session.go
@@ -3,10 +3,23 @@ package util
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"reflect"
+
+	"github.com/labstack/echo/v4"
 )
 
+// generate sessionID hash
 func GetSessionID(userAgent, acceptLang string) string {
 	sessionIDHash := sha256.Sum256([]byte(userAgent + acceptLang))
 	sessionID := hex.EncodeToString(sessionIDHash[:])
 	return sessionID
+}
+
+// get username from access token payload parsed from jwt middleware
+func GetUsername(c echo.Context) string {
+	payload := reflect.ValueOf(c.Get("user")).Elem()
+	claims := payload.FieldByName("Claims").Elem()
+	username := claims.Elem().FieldByName("Username")
+
+	return username.String()
 }
