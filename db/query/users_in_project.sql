4136074207a433df161a437967c1e306c04902cf
raman <ramanvahedi83@gmail.com>
2022-12-18 22:37:29 +0330

    add check if user in project

--- db/query/users_in_project.sql
+++ db/query/users_in_project.sql
@@ -13,6 +13,10 @@ WHERE user_id = $1;
 SELECT * FROM usersinproject
 WHERE project_id = $1;
 
+-- name: IsUserInProject :one
+SELECT * FROM usersinproject
+WHERE user_id = $1 AND project_id = $2;
+
 -- name: DeleteUserFromProject :exec
 DELETE FROM usersinproject
 WHERE user_id = $1 AND project_id = $2;
