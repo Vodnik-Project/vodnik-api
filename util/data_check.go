aee9d200996fc77b6560ed9634a8d588d04a3b72
raman <ramanvahedi83@gmail.com>
2022-12-19 15:30:47 +0330

    remove CheckPassword

--- util/data_check.go
+++ util/data_check.go
@@ -1,8 +1,6 @@
 package util
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
 	"reflect"
 )
@@ -14,14 +12,6 @@ func CheckEmpty(s interface{}, args []string) error {
 		if v.FieldByName(i).String() == "" {
 			return fmt.Errorf("%v can't be empty", i)
 		}
-	}
-	return nil
-}
-
-func CheckPassword(password, passHash string) error {
-	inputPassHash := sha256.Sum256([]byte(password))
-	if hex.EncodeToString(inputPassHash[:]) != passHash {
-		return fmt.Errorf("password is wrong")
 	}
 	return nil
 }
