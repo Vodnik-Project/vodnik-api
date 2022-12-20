34862ed3e1fedf95c15120d54bc6009472f3659e
raman <ramanvahedi83@gmail.com>
2022-12-20 23:36:12 +0330

    refactor json response schema + add logger

--- api/project.go
+++ api/project.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/Vodnik-Project/vodnik-api/db/sqlc"
+	log "github.com/Vodnik-Project/vodnik-api/logger"
 	"github.com/Vodnik-Project/vodnik-api/util"
 	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
@@ -16,77 +17,137 @@ type CreateProjectRequest struct {
 	Info  string `json:"info"`
 }
 
+type projectDataResponse struct {
+	ProjectID string `json:"project_id"`
+	Title     string `json:"title"`
+	Info      string `json:"info"`
+	OwnerID   string `json:"owner_id"`
+	CreatedAt string `json:"created_at"`
+}
+
 func (s Server) CreateProject(c echo.Context) error {
 	ctx := c.Request().Context()
 	var project CreateProjectRequest
 	err := c.Bind(&project)
 	if err != nil {
-		return c.JSON(http.StatusUnprocessableEntity, "can't bind data")
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("can't parse input data")
+		return c.JSON(http.StatusUnprocessableEntity, echo.Map{
+			"message": "invalid input",
+			"traceid": traceid,
+		})
 	}
-	err = util.CheckEmpty(project, []string{"Title", "Info"})
+	err = util.CheckEmpty(project, []string{"Title"})
 	if err != nil {
-		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg(err.Error())
+		return c.JSON(http.StatusUnprocessableEntity, echo.Map{
+			"message": err.Error(),
+			"traceid": traceid,
+		})
 	}
 	userid := util.GetFieldFromPayload(c, "UserID")
 	userUUID, err := uuid.FromString(userid)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, "can't parse uuid")
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("")
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "an error occurred while processing your request",
+			"traceid": traceid,
+		})
 	}
 	userID, err := s.store.GetUserById(ctx, userUUID)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, "can't get userid from db")
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("")
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "an error occurred while processing your request",
+			"traceid": traceid,
+		})
 	}
-	err = s.store.CreateProjectTx(ctx, sqlc.CreateProjectParams{
+	createdProject, err := s.store.CreateProjectTx(ctx, sqlc.CreateProjectParams{
 		Title:   project.Title,
 		Info:    sql.NullString{String: project.Info, Valid: true},
 		OwnerID: uuid.NullUUID{UUID: userID.UserID, Valid: true},
 	})
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, "can't create project")
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("")
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "an error occurred while processing your request",
+			"traceid": traceid,
+		})
 	}
-	return c.JSON(http.StatusOK, "project created successfully")
-}
-
-type GetProjectDataRespond struct {
-	Title     string `json:"title"`
-	Info      string `json:"info"`
-	Owner     string `json:"owner"`
-	CreatedAt string `json:"created_at"`
+	createdProjectData := createdProject.(sqlc.Project)
+	responseData := projectDataResponse{
+		ProjectID: createdProjectData.ProjectID.String(),
+		Title:     createdProjectData.Title,
+		Info:      createdProjectData.Info.String,
+		OwnerID:   createdProjectData.OwnerID.UUID.String(),
+		CreatedAt: createdProjectData.CreatedAt.Time.Format(time.RFC3339),
+	}
+	log.Logger.Info().Msgf("project created: %+v", responseData)
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "project created successfully",
+		"project": responseData,
+	})
 }
 
 func (s Server) GetProjectData(c echo.Context) error {
 	ctx := c.Request().Context()
 	projectID := c.Param("projectid")
 	userid := util.GetFieldFromPayload(c, "UserID")
-	userUUID, err := uuid.FromString(userid)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, "can't parse uuid")
-	}
-	user, err := s.store.GetUserById(ctx, userUUID)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
-	}
 	projectUUID, err := uuid.FromString(projectID)
 	if err != nil {
-		return c.JSON(http.StatusUnprocessableEntity, "projectid is wrong")
-	}
-	_, err = s.store.IsUserInProject(ctx, sqlc.IsUserInProjectParams{
-		UserID:    user.UserID,
-		ProjectID: projectUUID,
-	})
-	if err != nil {
-		return c.JSON(http.StatusUnauthorized, "can't access to project data")
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("")
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "an error occurred while processing your request",
+			"traceid": traceid,
+		})
 	}
 	project, err := s.store.GetProjectData(ctx, projectUUID)
 	if err != nil {
-		return c.JSON(http.StatusNotFound, "project not found")
+		traceid := util.RandomString(8)
+		if err == sql.ErrNoRows {
+			log.Logger.Err(err).Str("traceid", traceid).Msg("")
+			return c.JSON(http.StatusNotFound, echo.Map{
+				"message": "no project found",
+				"traceid": traceid,
+			})
+		}
 	}
-
-	return c.JSON(http.StatusOK, GetProjectDataRespond{
+	userUUID, err := uuid.FromString(userid)
+	if err != nil {
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("")
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "an error occurred while processing your request",
+			"traceid": traceid,
+		})
+	}
+	_, err = s.store.IsUserInProject(ctx, sqlc.IsUserInProjectParams{
+		UserID:    userUUID,
+		ProjectID: projectUUID,
+	})
+	if err != nil {
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("no access to project")
+		return c.JSON(http.StatusForbidden, echo.Map{
+			"message": "no access to project",
+			"traceid": traceid,
+		})
+	}
+	responseData := projectDataResponse{
+		ProjectID: project.ProjectID.String(),
 		Title:     project.Title,
 		Info:      project.Info.String,
-		Owner:     user.Username,
+		OwnerID:   project.OwnerID.UUID.String(),
 		CreatedAt: project.CreatedAt.Time.Format(time.RFC3339),
+	}
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "project found",
+		"project": responseData,
 	})
 }
 
