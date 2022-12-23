9567ca04af78bc0c3eb86856aaede93b0b9095b5
raman <ramanvhd@gmail.com>
2022-12-23 18:17:43 +0330

    remove unnecessary function

--- util/session.go
+++ util/session.go
@@ -3,9 +3,6 @@ package util
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"reflect"
-
-	"github.com/labstack/echo/v4"
 )
 
 // generate sessionID hash
@@ -13,13 +10,4 @@ func GetSessionID(userAgent, acceptLang string) string {
 	sessionIDHash := sha256.Sum256([]byte(userAgent + acceptLang))
 	sessionID := hex.EncodeToString(sessionIDHash[:])
 	return sessionID
-}
-
-// get field from access token payload parsed from jwt middleware
-func GetFieldFromPayload(c echo.Context, field string) string {
-	payload := reflect.ValueOf(c.Get("user")).Elem()
-	claims := payload.FieldByName("Claims").Elem()
-	username := claims.Elem().FieldByName(field)
-
-	return username.String()
 }
