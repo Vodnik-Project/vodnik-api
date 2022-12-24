aa1a18170c4355217725ba86f0d83d54e10ea72a
raman <ramanvhd@gmail.com>
2022-12-24 22:19:22 +0330

    add get task by filtering & sorting & pagination handler

--- api/task.go
+++ api/task.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Vodnik-Project/vodnik-api/util"
 	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
+	"gopkg.in/validator.v2"
 )
 
 type createTaskRequest struct {
@@ -164,8 +165,114 @@ func (s Server) GetTaskData(c echo.Context) error {
 	})
 }
 
+type getTaskRequest struct {
+	Title          string `json:"title"`
+	Info           string `json:"info"`
+	Tag            string `json:"tag"`
+	CreatedBy      string `json:"created_by"`
+	CreatedAtFrom  string `json:"created_at_from" validate:"regexp=(^$|^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))$)"`
+	CreatedAtUntil string `json:"created_at_until" validate:"regexp=(^$|^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))$)"`
+	BegginingFrom  string `json:"beggining_from" validate:"regexp=(^$|^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))$)"`
+	BegginingUntil string `json:"beggining_until" validate:"regexp=(^$|^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))$)"`
+	DeadlineFrom   string `json:"deadline_from" validate:"regexp=(^$|^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))$)"`
+	DeadlineUntil  string `json:"deadline_until" validate:"regexp=(^$|^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\\.[0-9]+)?(([Zz])|([\\+|\\-]([01][0-9]|2[0-3]):[0-5][0-9]))$)"`
+	Sortdirection  string `json:"sort_direction" validate:"regexp=(^$|asc|desc)"`
+	SortBy         string `json:"sort_by" validate:"regexp=(^$|beggining|deadline|created_at)"`
+	Limit          int32  `json:"limit"`
+	Page           int32  `json:"page"`
+}
+
 func (s Server) GetTasksByProjectID(c echo.Context) error {
-	return nil
+	ctx := c.Request().Context()
+	var taskRequest getTaskRequest
+	err := c.Bind(&taskRequest)
+	if err != nil {
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("can't parse input data")
+		return c.JSON(http.StatusUnprocessableEntity, echo.Map{
+			"message": "invalid input",
+			"traceid": traceid,
+		})
+	}
+	var createdBy uuid.UUID
+	if taskRequest.CreatedBy != "" {
+		createdBy, err = uuid.FromString(taskRequest.CreatedBy)
+		if err != nil {
+			traceid := util.RandomString(8)
+			log.Logger.Err(err).Str("traceid", traceid).Msg("invalid taskid")
+			return c.JSON(http.StatusUnprocessableEntity, echo.Map{
+				"message": "invalid created_by id",
+				"traceid": traceid,
+			})
+		}
+	}
+	if err = validator.Validate(taskRequest); err != nil {
+		traceid := util.RandomString(8)
+		log.Logger.Err(err).Str("traceid", traceid).Msg("invalid input data")
+		return c.JSON(http.StatusUnprocessableEntity, echo.Map{
+			"message": "invalid input data",
+			"error":   err.Error(),
+			"traceid": traceid,
+		})
+	}
+	createdAtFrom, _ := time.Parse(time.RFC3339, taskRequest.CreatedAtFrom)
+	createdAtUntil, _ := time.Parse(time.RFC3339, taskRequest.CreatedAtUntil)
+	begginingFrom, _ := time.Parse(time.RFC3339, taskRequest.BegginingFrom)
+	begginingUntil, _ := time.Parse(time.RFC3339, taskRequest.BegginingUntil)
+	deadlineFrom, _ := time.Parse(time.RFC3339, taskRequest.DeadlineFrom)
+	deadlineUntil, _ := time.Parse(time.RFC3339, taskRequest.DeadlineUntil)
+	projects, err := s.store.GetTasksByProjectID(ctx, sqlc.GetTasksByProjectIDParams{
+		ProjectID:      c.Get("projectUUID").(uuid.UUID),
+		Title:          taskRequest.Title,
+		Info:           taskRequest.Info,
+		Tag:            taskRequest.Tag,
+		CreatedBy:      createdBy,
+		CreatedAtFrom:  createdAtFrom,
+		CreatedAtUntil: createdAtUntil,
+		BegginingFrom:  begginingFrom,
+		BegginingUntil: begginingUntil,
+		DeadlineFrom:   deadlineFrom,
+		DeadlineUntil:  deadlineUntil,
+		Sortdirection:  taskRequest.Sortdirection,
+		Sortby:         taskRequest.SortBy,
+		Limit:          taskRequest.Limit,
+		Offset:         taskRequest.Page * taskRequest.Limit,
+	})
+	if err != nil {
+		traceid := util.RandomString(8)
+		if err == sql.ErrNoRows {
+			log.Logger.Err(err).Str("traceid", traceid).Msg("")
+			return c.JSON(http.StatusNotFound, echo.Map{
+				"message": "no task found",
+				"traceid": traceid,
+			})
+		}
+		log.Logger.Err(err).Str("traceid", traceid).Msg("")
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "an error occurred while processing your request",
+			"traceid": traceid,
+		})
+	}
+	var responseData []TaskDataResponse
+	for _, j := range projects {
+		responseData = append(responseData, TaskDataResponse{
+			TaskID:    j.TaskID.String(),
+			ProjectID: j.ProjectID.String(),
+			Title:     j.Title,
+			Info:      j.Info.String,
+			Tag:       j.Tag.String,
+			CreatedBy: j.CreatedBy.String(),
+			CreatedAt: j.CreatedAt.Time.Format(time.RFC3339),
+			Beggining: j.Beggining.Time.Format(time.RFC3339),
+			Deadline:  j.Deadline.Time.Format(time.RFC3339),
+			Color:     j.Color.String,
+		})
+	}
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "tasks found",
+		"count":   len(responseData),
+		"tasks":   responseData,
+	})
 }
 
 func (s Server) UpdateTask(c echo.Context) error {
